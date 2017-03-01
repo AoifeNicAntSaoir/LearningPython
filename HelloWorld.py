dd638f80def155bd447dfc886c6c5cd289a59ce6
Aoife Sayers <aoifesayers@gmail.com>
2017-03-01 09:45:41 +0000

    Added Python fie Learning print, variables raw_input String literals

---
+++ HelloWorld.py
@@ -0,0 +1,37 @@
+#print
+print "Hello, Python"
+
+#Continuation Character
+item1 = "Bread"
+item2 = "milk"
+item3 = "eggs"
+
+total = item1 + \
+        item2 + \
+        item3
+
+days = ["Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday"]
+
+#String literals
+word = 'word'
+sentence = """This is a sentence made up \
+           of multiple lines and sentences"""
+
+
+#Waiting for user input
+#raw_input("\n\nPress the key to exit\n")
+
+#variables
+#Standard data types: Number, String, List, Tuple, Dictionary
+counter = 100
+miles = 1000.50
+firstName = "Aoife"
+surname = 'Sayers'
+
+
+print "Hello my name is " + firstName + " " + surname
+#print "\nI drove " + miles + " miles today"
+
+age = raw_input("\nWhat is your age?\n")
+print age
